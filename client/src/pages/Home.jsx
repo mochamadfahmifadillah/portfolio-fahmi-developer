3e66a91f827c166f0a3a79df795026418966c72f
FahmiMoch <fahmifadillah18@gmail.com>
2026-05-02 17:01:14 +0700

    Menambahkan About

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import MainLayout from '../layouts/MainLayout'
 import Hero from '../sections/Hero'
+import About from '../sections/About'
 
 const Home = () => {
   return (
     <MainLayout>
       <Hero />
+      <About />
     </MainLayout>
   )
 }
