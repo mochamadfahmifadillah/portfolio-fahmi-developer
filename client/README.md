efbeb61a936aae111f214e9a50fbc70f5f05b1cc
FahmiMoch <fahmifadillah18@gmail.com>
2026-05-07 00:24:01 +0700

    Menambahkan loading untuk halaman

--- client/README.md
+++ client/README.md
@@ -1,3 +1,4 @@
 Portfolio Mochamad Fahmi Fadillah
 https://www.linkedin.com/in/mochamad-fahmi-fadillah/ My Linkedin
 https://github.com/mochamadfahmifadillah/ My Github
+https://mochamad-fahmi-fadillah.vercel.app/ Portfolio Fahmi
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,21 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import Home from './pages/Home'
+import Loading from './components/Loading'
+
 function App() {
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false)
+    }, 1500)
+
+    return () => clearTimeout(timer)
+  }, [])
+
+  if (loading) return <Loading />  
+
   return <Home />
 }
 
