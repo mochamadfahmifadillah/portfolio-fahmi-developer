18d33b2e9dc6ecd62d8bd89fcf47ee803645acc7
FahmiMoch <fahmifadillah18@gmail.com>
2026-05-09 00:30:29 +0700

    Perbaikan Navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,3 +1,10 @@
+import {
+  FaUser,
+  FaCode,
+  FaCertificate,
+  FaEnvelope,
+} from 'react-icons/fa'
+
 const Navbar = () => {
   return (
     <nav className="fixed top-0 left-0 w-full bg-[#362EED]/80 backdrop-blur-md border-b border-slate-800 z-50">
@@ -12,31 +19,29 @@ const Navbar = () => {
         <div className="flex items-center gap-6">
 
           {/* MENU */}
-          <div className="flex bg-[#e4e4e4] rounded-xl p-1">
+    <div className="navigation-card">
 
-            <button className="px-4 py-2 rounded-lg text-sm font-bold uppercase text-[#3035cb] bg-[#e4e4e4] shadow-[inset_5px_5px_5px_#c4c4c4,inset_-5px_-5px_5px_#ffffff] transition hover:bg-[#3035cb] hover:text-white hover:shadow-none">
-              About
-            </button>
+  {/* ABOUT */}
+  <a href="#about" className="tab about-tab">
+    <FaUser className="text-[22px]" />
+  </a>
 
-            <button className="px-4 py-2 rounded-lg text-sm font-bold uppercase text-[#333] bg-[#e4e4e4] shadow-[inset_5px_5px_5px_#c4c4c4,inset_-5px_-5px_5px_#ffffff] transition hover:bg-[#333] hover:text-white hover:shadow-none">
-              Projects
-            </button>
+  {/* PROJECTS */}
+  <a href="#projects" className="tab projects-tab">
+    <FaCode className="text-[22px]" />
+  </a>
 
-            <button className="px-4 py-2 rounded-lg text-sm font-bold uppercase text-[#333] bg-[#e4e4e4] shadow-[inset_5px_5px_5px_#c4c4c4,inset_-5px_-5px_5px_#ffffff] transition hover:bg-[#333] hover:text-white hover:shadow-none">
-              Certificates
-            </button>
+  {/* CERTIFICATES */}
+  <a href="#certificate" className="tab certificates-tab">
+    <FaCertificate className="text-[22px]" />
+  </a>
 
-            <button className="px-4 py-2 rounded-lg text-sm font-bold uppercase text-[#b82323] bg-[#e4e4e4] shadow-[inset_5px_5px_5px_#c4c4c4,inset_-5px_-5px_5px_#ffffff] transition hover:bg-[#b82323] hover:text-white hover:shadow-none">
-              Contact
-            </button>
+  {/* CONTACT */}
+  <a href="#contact" className="tab contact-tab">
+    <FaEnvelope className="text-[22px]" />
+  </a>
 
-          </div>
-
-          {/* SWITCH (OUTSIDE MENU) */}
-          <label className="switch">
-            <input type="checkbox" />
-            <span className="slider"></span>
-          </label>
+</div>
 
         </div>
 
